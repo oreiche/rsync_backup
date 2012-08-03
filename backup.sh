892db40f6797378abd85b593b8a6d4cbc6c60613
ofp <none@none>
2012-08-03 19:29:02 +0200

    Refactoring: Refactored function names and configuration variables

--- backup.sh
+++ backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+#
+# (C) Oliver Reiche <oliver.reiche@siemens.com>
 
-g_backupdir="/backup/"
-g_sourcedir="/"
-g_excludedirs=("dev" "mnt" "tmp")
+conf_sourcedir="/"
+conf_backupdir="/backup"
+conf_excludedirs=("dev" "mnt" "tmp")
 
-g_default="day"
-g_snapshots="6"
+conf_name="day"
+conf_snapshots="6"
 
-g_stages=(
+conf_stages=(
     #NAME      MINUTES  SNAPSHOTS
     "week"      "10080" "3"
     "month"     "40320" "2"
@@ -16,120 +18,133 @@ g_stages=(
     "year"     "483840" "3"
 )
 
+################################################################################
+
 g_timestamp=$(date +%s)
 
 function checkTimestamp() {
-    local curr=${g_stages[$1]}
-    local minutes=${g_stages[$(($1+1))]}
-    local timestamp=$(cat $g_backupdir/$curr.stamp 2>/dev/null)
+    local curr=${conf_stages[$1]}
+    local minutes=${conf_stages[$(($1+1))]}
+    local timestamp=$(cat $conf_backupdir/$curr.stamp 2>/dev/null)
     local retval=1
 
     if [ "$timestamp" == "" ]; then
-        echo $g_timestamp > $g_backupdir/$curr.stamp
+        echo $g_timestamp > $conf_backupdir/$curr.stamp
     elif [ $((($(date +%s) - $timestamp) / 60)) -ge $minutes ]; then
-        echo $g_timestamp > $g_backupdir/$curr.stamp
+        echo $g_timestamp > $conf_backupdir/$curr.stamp
         retval=0
     fi
 
     return $retval
 }
 
+################################################################################
+
 function createStage() {
-    local curr=${g_stages[$1]}
+    local curr=${conf_stages[$1]}
     local prev
     local last
 
     if [ $1 -ge 3 ]; then
-        prev=${g_stages[$(($1-3))]}
+        prev=${conf_stages[$(($1-3))]}
     else
-        prev=$g_default
+        prev=$conf_name
     fi
-    last="$(ls $g_backupdir/ | grep $prev | sort -n -t '.' -k 2 | tail -n1)"
+    last="$(ls $conf_backupdir/ | grep $prev | sort -n -t '.' -k 2 | tail -n1)"
 
     if [ "$last" != "" ] && 
        [ "$last" != $prev.0 ]; then
         echo "Creating new snapshot for stage '$curr'."
-        rm -rf $g_backupdir/$curr.1
-        mv $g_backupdir/$last $g_backupdir/$curr.1 2>/dev/null
+        rm -rf $conf_backupdir/$curr.1
+        mv $conf_backupdir/$last $conf_backupdir/$curr.1 2>/dev/null
     fi
 }
 
+################################################################################
+
 function shiftStage() {
-    local curr=${g_stages[$1]}
-    local i=${g_stages[$(($1+2))]}
+    local curr=${conf_stages[$1]}
+    local i=${conf_stages[$(($1+2))]}
 
-    echo "Shifting stage '$curr'."
-
-    rm -rf $g_backupdir/$curr.$i
-    while [ $i -gt 1 ]; do
-        mv $g_backupdir/$curr.$(($i-1)) $g_backupdir/$curr.$i 2>/dev/null
-        i=$(($i-1))
-    done
+    if [ -d $conf_backupdir/$curr.1 ]; then
+        echo "Shifting snapshots of stage '$curr'."
+        rm -rf $conf_backupdir/$curr.$i
+        while [ $i -gt 1 ]; do
+            mv $conf_backupdir/$curr.$(($i-1)) $conf_backupdir/$curr.$i 2>/dev/null
+            i=$(($i-1))
+        done
+    fi
 }
 
-function createDefault() {
-    echo "Creating new snapshot for default stage '$g_default'."
+################################################################################
 
-    if [ -d $g_backupdir/$g_default.tmp ]; then
+function createInit() {
+    echo "Creating new snapshot for initial stage '$conf_name'."
+
+    if [ -d $conf_backupdir/$conf_name.tmp ]; then
         # Reuse the temp directory
-        mv $g_backupdir/$g_default.0 $g_backupdir/$g_default.1
-        mv $g_backupdir/$g_default.tmp $g_backupdir/$g_default.0
+        mv $conf_backupdir/$conf_name.0 $conf_backupdir/$conf_name.1
+        mv $conf_backupdir/$conf_name.tmp $conf_backupdir/$conf_name.0
         if [[ $OSTYPE == *darwin* ]]; then
-            cd $g_backupdir/$g_default.1
-            find . -print | cpio -pdlm $g_backupdir/$g_default.0 2>/dev/null
+            cd $conf_backupdir/$conf_name.1
+            find . -print | cpio -pdlm $conf_backupdir/$conf_name.0 2>/dev/null
         else
-            cp -al $g_backupdir/$g_default.1/. $g_backupdir/$g_default.0
+            cp -al $conf_backupdir/$conf_name.1/. $conf_backupdir/$conf_name.0
         fi
-    elif [ -d $g_backupdir/$g_default.0 ]; then
-        rm -rf $g_backupdir/$g_default.1
+    elif [ -d $conf_backupdir/$conf_name.0 ]; then
+        rm -rf $conf_backupdir/$conf_name.1
         if [[ $OSTYPE == *darwin* ]]; then
-            cd $g_backupdir/$g_default.0
-            find . -print | cpio -pdlm $g_backupdir/$g_default.1 2>/dev/null
+            cd $conf_backupdir/$conf_name.0
+            find . -print | cpio -pdlm $conf_backupdir/$conf_name.1 2>/dev/null
         else
-            cp -al $g_backupdir/$g_default.0 $g_backupdir/$g_default.1
+            cp -al $conf_backupdir/$conf_name.0 $conf_backupdir/$conf_name.1
         fi
     fi
 
     local excluded
-    local exclude=$(echo $g_backupdir | grep $g_sourcedir | \
-                    sed "s/^$(echo $g_sourcedir | sed 's/\//\\\//g')\///g")
+    local exclude=$(echo $conf_backupdir | grep $conf_sourcedir | \
+                    sed "s/^$(echo $conf_sourcedir | sed 's/\//\\\//g')\///g")
     if [ "$exclude" != "" ]; then
         excluded="$excluded --exclude=$exclude"
     fi
-    for exclude in "${g_excludedirs[@]}"; do
+    for exclude in "${conf_excludedirs[@]}"; do
         excluded="$excluded --exclude=$exclude"
     done
 
-    rsync -a --delete $excluded $g_sourcedir/ $g_backupdir/$g_default.0/
+    rsync -a --delete $excluded $conf_sourcedir/ $conf_backupdir/$conf_name.0/
 }
 
-function shiftDefault() {
-    local i=$g_snapshots
+################################################################################
 
-    echo "Shifting default stage '$g_default'."
+function shiftInit() {
+    local i=$conf_snapshots
 
-    if [ -d $g_backupdir/$g_default.$i ]; then
+    echo "Shifting snapshots of initial stage '$conf_name'."
+
+    if [ -d $conf_backupdir/$conf_name.$i ]; then
         # Store as temp, this speeds up the whole operation
-        mv $g_backupdir/$g_default.$i $g_backupdir/$g_default.tmp
+        mv $conf_backupdir/$conf_name.$i $conf_backupdir/$conf_name.tmp
     fi
 
     while [ $i -gt 1 ]; do
-        mv $g_backupdir/$g_default.$(($i-1)) $g_backupdir/$g_default.$i 2>/dev/null
+        mv $conf_backupdir/$conf_name.$(($i-1)) $conf_backupdir/$conf_name.$i 2>/dev/null
         i=$(($i-1))
     done
 }
 
+################################################################################
+
 function main() {
-    local n=${#g_stages[*]}
+    local n=${#conf_stages[*]}
 
     if [ $(($n % 3)) -ne 0 ]; then
         echo "Configuration error: Malformed stages array."
         exit 1
-    elif [ ! -d $g_sourcedir ]; then
-        echo "Directory '$g_sourcedir' does not exist."
+    elif [ ! -d $conf_sourcedir ]; then
+        echo "Directory '$conf_sourcedir' does not exist."
         exit 1
-    elif [ ! -d $g_backupdir ]; then
-        echo "Directory '$g_backupdir' does not exist."
+    elif [ ! -d $conf_backupdir ]; then
+        echo "Directory '$conf_backupdir' does not exist."
         exit 1
     else
         local i=$(($n - 3))
@@ -142,12 +157,15 @@ function main() {
             i=$(($i-3))
         done
 
-        shiftDefault
-        createDefault
+        shiftInit
+        createInit
     fi
 
     exit 0
 }
 
 main
+
+################################################################################
+################################################################################
 
